a1d54b2e1c82e506df0a2a7ab6043465faa30cba
Ken Moini <ken@kenmoini.com>
2023-10-31 20:30:55 -0400

    fffffffffff

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+# Splunk Searches
+
+```
+
+source="http:ocp" log_type=application level=debug
+
+```
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -32,6 +33,30 @@ func isDebug() bool {
 	}
 }
 
+type jsonMessage struct {
+	Host      string `json:"host"`
+	Message   string `json:"message"`
+	PID       int    `json:"pid"`
+	TID       int    `json:"tid"`
+	Timestamp string `json:"timestamp"`
+}
+type jsonObjectMarshaler struct {
+	obj any
+}
+
+func (j *jsonObjectMarshaler) MarshalJSON() ([]byte, error) {
+	bytes, err := json.Marshal(j.obj)
+	// bytes, err := protojson.Marshal(j.obj)
+	if err != nil {
+		return nil, fmt.Errorf("json marshaling failed: %w", err)
+	}
+	return bytes, nil
+}
+
+func ZapJsonable(key string, obj any) zap.Field {
+	return zap.Reflect(key, &jsonObjectMarshaler{obj: obj})
+}
+
 func main() {
 	rawJSON := []byte(`{
 		"level": "debug",
@@ -57,26 +82,29 @@ func main() {
 	}
 	defer logger.Sync() // flushes buffer, if any
 
+	// Setup some sugar
+	sugar := logger.Sugar()
+
 	// Read in the environmental variable for INTERVAL
 	interval, err := time.ParseDuration(SetDefaultString("5s", os.Getenv("INTERVAL")))
 	if err != nil {
-		logger.Fatal(err.Error())
+		sugar.Fatal(err.Error())
 	}
 	if isDebug() {
-		logger.Info("Interval", zap.String("interval", interval.String()))
+		sugar.Infow("Interval", "interval", interval.String())
 	}
 
 	// Read in the environmental variable for MESSAGE_PATH
 	messagePath := SetDefaultString("./messages", os.Getenv("MESSAGE_PATH"))
 	if isDebug() {
-		logger.Info("Message Path", zap.String("messagePath", messagePath))
+		sugar.Infow("Message Path", "messagePath", messagePath)
 	}
 
 	// Find all the files in the MESSAGE_PATH directory
 	// that match a .msg extension
 	files, err := os.ReadDir(messagePath)
 	if err != nil {
-		log.Fatal(err.Error())
+		sugar.Fatal(err.Error())
 	}
 
 	// Start an interval loop
@@ -94,27 +122,37 @@ func main() {
 			// Make sure this is not a directory
 			if !file.IsDir() {
 				if isDebug() {
-					logger.Info("Processing message file", zap.String("file", file.Name()))
+					sugar.Infow("Processing message file", "file", file.Name())
 				}
 
 				// Remove the .msg extension from the file name
 				fileBaseName := strings.TrimSuffix(file.Name(), ".msg")
 				if isDebug() {
-					logger.Info("Processing message file", zap.String("file", fileBaseName))
+					sugar.Infow("Processing message file", "file", fileBaseName)
 				}
 
 				// Split the filename at the hyphen
 				fileNameParts := strings.Split(fileBaseName, "-")
 				if isDebug() {
-					logger.Info("File name parts", zap.Strings("fileNameParts", fileNameParts))
+					sugar.Infow("File name parts", "fileNameParts", fileNameParts)
 				}
 
 				if len(fileNameParts) > 1 {
 					// Set the log info variables
-					//logType := fileNameParts[0]
+					logType := fileNameParts[0]
+					if isDebug() {
+						sugar.Infow("Log type", "logType", logType)
+					}
+					// Split the log type by underscores
+					logTypeParts := strings.Split(logType, "_")
+					if isDebug() {
+						sugar.Infow("Log type parts", "logTypeParts", logTypeParts)
+					}
+
+					// Set the log level
 					logLevel := fileNameParts[1]
 					if isDebug() {
-						logger.Info("Log level", zap.String("logLevel", logLevel))
+						sugar.Infow("Log level", "logLevel", logLevel)
 					}
 
 					// Read in the file data
@@ -123,17 +161,53 @@ func main() {
 						log.Fatal(err)
 					}
 
-					// Log the message
-					switch logLevel {
-					case "debug":
-						logger.Debug(string(fileData))
-					case "info":
-						logger.Info(string(fileData))
-					case "warn":
-						logger.Warn(string(fileData))
-					case "error":
-					case "err":
-						logger.Error(string(fileData))
+					if len(logTypeParts) > 1 {
+						switch logTypeParts[0] {
+						case "json":
+							jsonData := jsonMessage{}
+							err = json.Unmarshal(fileData, &jsonData)
+
+							if err != nil {
+								sugar.Fatal(err.Error())
+							} else {
+								switch logLevel {
+								case "debug":
+									sugar.Debugw(file.Name(), ZapJsonable("event", jsonData))
+								case "info":
+									sugar.Infow(file.Name(), ZapJsonable("event", jsonData))
+								case "warn":
+									sugar.Warnw(file.Name(), ZapJsonable("event", jsonData))
+								case "error":
+								case "err":
+									sugar.Errorw(file.Name(), ZapJsonable("event", jsonData))
+								}
+							}
+						case "text":
+							switch logLevel {
+							case "debug":
+								sugar.Debug(string(fileData))
+							case "info":
+								sugar.Info(string(fileData))
+							case "warn":
+								sugar.Warn(string(fileData))
+							case "error":
+							case "err":
+								sugar.Error(string(fileData))
+							}
+						}
+					} else {
+						// Log the message
+						switch logLevel {
+						case "debug":
+							logger.Debug(string(fileData))
+						case "info":
+							logger.Info(string(fileData))
+						case "warn":
+							logger.Warn(string(fileData))
+						case "error":
+						case "err":
+							logger.Error(string(fileData))
+						}
 					}
 				}
 
